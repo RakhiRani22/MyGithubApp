73103c52209980a302d8404b399d8ee868179c52
Rakhi Rani <rakhi.rani22@gmail.com>
2021-10-17 14:45:39 +0200

    Created a new activity to display the commit list

---
+++ app/src/main/java/com/myapp/mygithubapp/ui/CommitInfoActivity.java
@@ -0,0 +1,14 @@
+package com.myapp.mygithubapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.myapp.mygithubapp.R;
+
+public class CommitInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_commit_information);
+    }
+}
